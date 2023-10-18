c97cb0ca292275643dfdf5f579d9aae14376d0db
Yifeng He <yfhe@ucdavis.edu>
2023-10-18 13:55:59 -0700

    add ast util on tree-sitter parsed AST

--- frontend/parser/ast_util.py
+++ frontend/parser/ast_util.py
@@ -1,60 +1,50 @@
 from tree_sitter import Language, Parser, Tree
-from tree_sitter.binding import Node, Range
+from tree_sitter.binding import Node
 from returns.maybe import Maybe, Nothing, Some
 from unitsyncer.common import UNITSYNCER_HOME
 from frontend.parser.langauges import JAVA_LANGUAGE
 
 
-def get_source_from_node(source_code: str, node: Node) -> str:
-    start = node.start_byte
-    end = node.end_byte
-    return source_code[start:end]
+class ASTUtil:
+    def __init__(self, source_code: str) -> None:
+        self.src = source_code
 
+    def tree(self, lang: Language) -> Tree:
+        parser = Parser()
+        parser.set_language(lang)
+        return parser.parse(bytes(self.src, "utf8"))
 
-def get_method_name(source_code: str, method_node: Node) -> Maybe[str]:
-    if method_node.type != "method_declaration":
+    def get_source_from_node(self, node: Node) -> str:
+        start = node.start_byte
+        end = node.end_byte
+        return self.src[start:end]
+
+    def get_method_name(self, method_node: Node) -> Maybe[str]:
+        if method_node.type != "method_declaration":
+            return Nothing
+
+        # for a method decl, its name is the first identifier
+        for child in method_node.children:
+            if child.type == "identifier":
+                return Some(self.get_source_from_node(child))
+
         return Nothing
 
-    # for a method decl, its name is the first identifier
-    for child in method_node.children:
-        if child.type == "identifier":
-            return Some(get_source_from_node(source_code, child))
+    def get_method_modifiers(self, method_node: Node) -> Maybe[list[str]]:
+        if method_node.type != "method_declaration":
+            return Nothing
 
-    return Nothing
+        modifiers = []
+        for child in method_node.children:
+            if child.type == "modifiers":
+                for modifier_child in child.children:
+                    modifiers.append(self.get_source_from_node(modifier_child))
+        return Some(modifiers)
 
-
-def tree_walker(source_code: str):
-    def walk_tree(node):
-        if node.type == "method_declaration":
-            print(get_method_name(source_code, node).value_or(None))
-            # print(extract_source_from_node(node, src))
-        for child in node.children:
-            walk_tree(child)
-
-    return walk_tree
-
-
-def get_all_nodes_of_type(root: Node, type: str) -> list[Node]:
-    nodes = []
-    for child in root.children:
-        if child.type == type:
-            nodes.append(child)
-        nodes += get_all_nodes_of_type(child, type)
-    return nodes
-
-
-def main():
-    file_path = f"{UNITSYNCER_HOME}/data/repos/spring-cloud-spring-cloud-netflix/spring-cloud-spring-cloud-netflix-630151f/spring-cloud-netflix-eureka-client/src/test/java/org/springframework/cloud/netflix/eureka/http/AbstractEurekaHttpClientTests.java"
-    with open(file_path) as f:
-        src = f.read()
-    parser = Parser()
-    parser.set_language(JAVA_LANGUAGE)
-    walk_tree = tree_walker(src)
-
-    tree = parser.parse(bytes(src, "utf8"))
-    root_node = tree.root_node
-    walk_tree(root_node)
-
-
-if __name__ == "__main__":
-    main()
+    def get_all_nodes_of_type(self, root: Node, type: str) -> list[Node]:
+        nodes = []
+        for child in root.children:
+            if child.type == type:
+                nodes.append(child)
+            nodes += self.get_all_nodes_of_type(child, type)
+        return nodes
