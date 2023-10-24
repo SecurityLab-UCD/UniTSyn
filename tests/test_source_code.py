921328b394dc39bf1a1ba14cd8e7dcd6958cd79e
Yifeng He <yfhe@ucdavis.edu>
2023-10-24 16:36:47 -0700

    update python test

--- tests/test_source_code.py
+++ tests/test_source_code.py
@@ -1,7 +1,7 @@
 import unittest
 import os
 import logging
-from unitsyncer.source_code import python_get_function_code
+from unitsyncer.source_code import get_function_code
 from returns.maybe import Nothing, Some
 from pylspclient.lsp_structs import Location, Position, Range
 
@@ -11,16 +11,16 @@ class TestSourceCode(unittest.TestCase):
         uri = f"file://{os.getcwd()}/data/repos/py_example/src/add.py"
         range_ = Range(Position(0, 4), Position(0, 7))
         loc = Location(uri, range_)
-        add_src = "\n\ndef add(x: int, y: int) -> int:\n    return (x + y)\n"
+        add_src = "def add(x: int, y: int) -> int:\n    return x + y"
 
-        self.assertEqual(python_get_function_code(loc), Some(add_src))
+        self.assertEqual(get_function_code(loc, "python").unwrap()[0], add_src)
 
     def test_python_get_function_code_not_found(self):
         uri = f"file://{os.getcwd()}/data/repos/py_example/src/add.py"
         range_ = Range(Position(1, 4), Position(1, 7))
         loc = Location(uri, range_)
 
-        self.assertEqual(python_get_function_code(loc), Nothing)
+        self.assertEqual(get_function_code(loc, "python"), Nothing)
 
 
 if __name__ == "__main__":
