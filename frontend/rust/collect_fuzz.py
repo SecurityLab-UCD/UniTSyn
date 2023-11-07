394f387665d41f7181f5fb7c1d8b1b2aad8757e8
Yifeng He <yfhe@ucdavis.edu>
2023-11-06 17:02:52 -0800

    add fuzz pipeline for all repos

--- frontend/rust/collect_fuzz.py
+++ frontend/rust/collect_fuzz.py
@@ -4,6 +4,7 @@ import fire
 import os
 from frontend.util import wrap_repo, parallel_subprocess
 import subprocess
+from os.path import join as pjoin, basename, splitext as psplitext, abspath
 
 
 def transform_repos(repos: list[str], jobs: int):
@@ -18,10 +19,87 @@ def transform_repos(repos: list[str], jobs: int):
     parallel_subprocess(repos, jobs, transform_one_repo, on_exit=None)
 
 
+def get_target_list(p: subprocess.Popen):
+    match p.stdout:
+        case None:
+            return []
+        case _:
+            return p.stdout.read().decode("utf-8").split("\n")
+
+
+def fuzz_one_target(target: tuple[str, str], timeout):
+    repo_path, target_name = target
+    with open(pjoin(repo_path, "fuzz_inputs", target_name), "w") as f:
+        return subprocess.Popen(
+            # todo: find out why -max_total_time doesn't work
+            # ["cargo", "fuzz", "run", target_name, "--", f"-max_total_time={timeout}"],
+            [
+                "bash",
+                "-c",
+                f"timeout {timeout} cargo fuzz run {target_name}",
+            ],
+            cwd=repo_path,
+            stdout=f,
+            stderr=subprocess.DEVNULL,
+        )
+
+
+def fuzz_repos(repos: list[str], jobs: int, timeout: int = 60):
+    logging.info(f"Initializing fuzzing targets in {len(repos)} repos")
+    parallel_subprocess(
+        repos,
+        jobs,
+        lambda path: subprocess.Popen(
+            ["cargo", "fuzz", "init"],
+            cwd=path,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        ),
+        on_exit=None,
+    )
+    logging.info(f"Building fuzzing targets in {len(repos)} repos")
+    parallel_subprocess(
+        repos,
+        jobs,
+        lambda path: subprocess.Popen(
+            ["cargo", "fuzz", "build"],
+            cwd=path,
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
+        ),
+        on_exit=None,
+    )
+
+    logging.info("Collecting all fuzz targets")
+
+    target_map = parallel_subprocess(
+        repos,
+        jobs,
+        lambda path: subprocess.Popen(
+            ["cargo", "fuzz", "list"], cwd=path, stdout=subprocess.PIPE
+        ),
+        on_exit=get_target_list,
+    )
+    targets: list[tuple[str, str]] = [
+        (k, v) for k, vs in target_map.items() for v in vs if len(v) > 0
+    ]
+    for repo in repos:
+        os.makedirs(pjoin(repo, "fuzz_inputs"), exist_ok=True)
+
+    logging.info(f"Running cargo fuzz on {len(targets)} targets for {timeout} seconds")
+    parallel_subprocess(
+        targets, jobs, lambda p: fuzz_one_target(p, timeout), on_exit=None
+    )
+
+
+def test_gen_repos(repos: list[str], jobs: int):
+    pass
+
+
 def main(
     repo_id: str = "marshallpierce/rust-base64",
     repo_root: str = "data/rust_repos/",
-    timeout: int = 120,
+    timeout: int = 60,
     nprocs: int = 0,
     limits: Optional[int] = None,
     pipeline: str = "transform",
@@ -46,11 +124,15 @@ def main(
                 for d in os.listdir(repo_path)
                 if os.path.isdir(os.path.join(repo_path, d))
             ]
-            repos.append(subdirectories[0])
+            repos.append(abspath(subdirectories[0]))
 
     match pipeline:
         case "transform":
             transform_repos(repos, nprocs)
+        case "fuzz":
+            fuzz_repos(repos, nprocs, timeout=timeout)
+        case "testgen":
+            test_gen_repos(repos, nprocs)
         case _:
             logging.error(f"Unknown pipeline {pipeline}")
 
