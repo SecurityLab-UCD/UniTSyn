32e2b9ea0073e0520b921a0cc52d6984c9b61981
HockJC <56396327+HockJC@users.noreply.github.com>
2023-11-07 00:10:38 -0800

    create finding rust repos files

--- scripts/find_rust_repos.py
+++ scripts/find_rust_repos.py
@@ -1,30 +1,12 @@
 """
-Finds new repo(s) of the specified language and checks if the repo(s) meet requirements
+Finds new Rust repo(s) and checks if the repo(s) meet requirements
 
-References
-
-Search query string:
-https://docs.github.com/en/search-github/github-code-search/understanding-github-code-search-syntax
-
-For path querying specifically:
-https://docs.github.com/en/search-github/github-code-search/understanding-github-code-search-syntax#path-qualifier
 """
 import sys
-
 from check_repo_stats import check_requirements
 from common import get_graphql_data
 
-
-def find_repos(requirements: list[callable], reqs: list[str]) -> None:
-    """Searches for new repos based on query parameters and checks if they fit the requirements given while resources are available
-
-    Args:
-        search_query (list[str]): queries to narrow down the search
-        requirements (list[callable]): List of requirement callables that check if repo meets requirement
-        reqs (list[str]): List of values to check against for each callable
-            - each req will be called with the callable of the same index in requirements
-    """
-
+def find_rust_repos(requirements: list[callable], reqs: list[str]) -> None:
     # Read in last used cursor
     with open(f"{language}_cursor.txt", "r") as f:
         cursor = f.read().strip()
@@ -56,6 +38,14 @@ def find_repos(requirements: list[callable], reqs: list[str]) -> None:
                             totalCount
                         }
                         pushedAt
+                        object(expression: "HEAD:") {
+                            ... on Tree {
+                                entries {
+                                    name
+                                    type
+                                }
+                            }
+                        }
                     }
                 }
             }
@@ -66,8 +56,7 @@ def find_repos(requirements: list[callable], reqs: list[str]) -> None:
     bulk_size = 50
     # Can maybe specify the requirements in the search query
     # Doesn't look like latest commit can be specified in the search query
-    # Just as an example:
-    language = '"python"'  # Surround language with double quotes for exact search
+    language = '"Rust"'  # Surround language with double quotes for exact search
     stars = 10
 
     search_query: str = f"language:{language} stars:>{stars} sort:stars"
@@ -82,6 +71,7 @@ def find_repos(requirements: list[callable], reqs: list[str]) -> None:
     for repo in repos["data"]["search"]["edges"]:
         repo_name = f"{repo['owner']['login']}/{repo['name']}"
         if check_requirements(repo_name, requirements, reqs):
+            print(f"Repository {repo_name} meets the requirements.")
             repos_to_save.append(repo_name)
 
     # Write each repo to a file
@@ -89,7 +79,13 @@ def find_repos(requirements: list[callable], reqs: list[str]) -> None:
 
     # Update the cursor for the next execution
     with open(f"{language.strip('"')}_cursor.txt", "w") as f:
-        f.write(new_cursor)
+        f.write(new_cursor)   
+
+
+# define requirements and reqs list
+requirements = [req_enough_stars, req_latest_commit, req_language, req_fuzzers]
+reqs = ['10', '2021-01-01', 'Rust', None]  # reqs that satisfied with rust
+
 
 def save_repos_to_file(language: str, repos_list: list[str]) -> None:
     """Save the repository names to a file named <language>.txt in the ../data/repo_meta directory."""
@@ -98,3 +94,5 @@ def save_repos_to_file(language: str, repos_list: list[str]) -> None:
         for repo_name in repos_list:
             file.write(repo_name + "\n")
 
+if __name__ == "__main__":
+    find_rust_repos(requirements, reqs)
