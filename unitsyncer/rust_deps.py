1589a11197a26238007c71db24e379c780e54493
Yifeng He <yfhe@ucdavis.edu>
2023-11-10 14:03:22 -0800

    add alternate FakeLSP for rust

--- unitsyncer/rust_deps.py
+++ unitsyncer/rust_deps.py
@@ -1,52 +1,104 @@
-import re
-import subprocess
+from pip._vendor import tomli
+from os.path import join as pjoin, isfile, isdir, abspath
+import os
+from returns.maybe import Maybe, Nothing, Some
+from returns.result import Result, Success, Failure
+from frontend.parser.ast_util import ASTLoc, ASTUtil
+from frontend.parser.langauges import RUST_LANGUAGE
+from tree_sitter import Language, Parser, Tree
+from tree_sitter.binding import Node
+from pylspclient.lsp_structs import Location, LANGUAGE_IDENTIFIER, Range, Position
+from unitsyncer.source_code import get_function_code
+from unitsyncer.util import path2uri, uri2path
+from returns.converters import maybe_to_result
 
 
-def process(raw_stat: str) -> list[str]:
-    """process the raw `rust-analyzer analysis-stats` to a list of function dependencies
+class FakeRustLSP:
+    def __init__(self, workdir: str) -> None:
+        self.workdir = workdir
+        self.file_func_map: dict[str, list[tuple[str, Node]]] = {}
 
-    Args:
-        raw_stat (str): stdout of `rust-analyzer analysis-stats`
+    def initialize(self):
+        """index all files and functions in the workdir/src"""
+        src_dir = pjoin(self.workdir, "src")
+        for root, dirs, files in os.walk(src_dir):
+            for file in files:
+                if file.endswith(".rs"):
+                    file_path = pjoin(root, file)
+                    funcs = self._get_file_functions(file_path)
+                    self.file_func_map[file_path] = funcs
 
-    Returns:
-        list[str]: list of function dependencies
-    """
-    # The regex pattern to match "{number}/{number} {number}% processing: "
-    pattern = r"\d+/\d+ \d+% processing: "
+    def _get_file_functions(self, file_path: str) -> list[tuple[str, Node]]:
+        """get all function items in the given file
 
-    output_string = re.sub(pattern, "\n", raw_stat)
-    return [l.strip() for l in output_string.split("\n")]
+        Args:
+            file_path (str): path to source code file
 
+        Returns:
+            list[tuple[str, Node]]: [(function_name, function_node)]
+        """
+        ast_util = ASTUtil(open(file_path).read())
+        tree = ast_util.tree(RUST_LANGUAGE)
+        nodes = ast_util.get_all_nodes_of_type(tree.root_node, "function_item")
+        names = [ast_util.get_name(node).value_or("") for node in nodes]
+        return list(zip(names, nodes))
 
-def get_analysis_stats(workdir: str) -> list[str]:
-    """get workdir's function dependencies analysis
+    # todo: align type with sync.py
+    def get_source_of_call(
+        self, focal_name: str
+    ) -> Result[tuple[str, str | None, str | None], str]:
+        match self.goto_definition(focal_name):
+            case [] | None:
+                return Failure("Not Definition Found")
+            case [loc, *_]:
+                # todo: find best match based on imports of test file
 
-    Args:
-        workdir (str): path to workspace folder
+                def not_found_error(_):
+                    file_path = uri2path(loc.uri).value_or("")
+                    lineno = loc.range.start.line
+                    col_offset = loc.range.start.character
+                    return f"Source code not found: {file_path}:{lineno}:{col_offset}"
 
-    Returns:
-        list[str]: list of function dependencies
-    """
-    p = subprocess.run(
-        ["rust-analyzer", "analysis-stats", workdir],
-        capture_output=True,
-        text=True,
-    )
-    raw_data = p.stdout.replace("\x08", "")
-    return process(raw_data)
+                return (
+                    maybe_to_result(get_function_code(loc, LANGUAGE_IDENTIFIER.RUST))
+                    .alt(not_found_error)
+                    .bind(
+                        lambda t: Failure("Empty Source Code")
+                        if t[0] == ""
+                        else Success(t)
+                    )
+                )
+            case _:
+                return Failure("Unexpected Error")
+
+    def goto_definition(self, focal_name: str) -> list[Location]:
+        """get the definition of the given function name
+
+        Args:
+            focal_name (str): name of the function
+
+        Returns:
+            list[Location]: all locations of function definition with focal_name
+        """
+        results = []
+        for file_path, funcs in self.file_func_map.items():
+            for name, node in funcs:
+                if name == focal_name:
+                    uri = path2uri(file_path)
+                    range_ = Range(
+                        Position(*node.start_point),
+                        Position(*node.end_point),
+                    )
+                    results.append(Location(uri, range_))
+        return results
 
 
 def main():
-    # with open("stat") as f:
-    #     raw_stat = f.read().replace("\x08", "")
-    # # stat = stat.split("processing: ")
-    # deps = process(raw_stat)
-    # print(len(deps))
-    # print(deps)
     workdir = "data/repos/marshallpierce-rust-base64/marshallpierce-rust-base64-4ef33cc"
-    stats = get_analysis_stats(workdir)
-    print(len(stats))
-    print(stats)
+    lsp = FakeRustLSP(workdir)
+    lsp.initialize()
+
+    print(lsp.get_source_of_call("decode"))
 
 
 if __name__ == "__main__":
