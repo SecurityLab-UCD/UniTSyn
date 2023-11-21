8a2a6e0cbc142af19bc517878ea3b11b10b147d3
HOCKJC <791064944@qq.com>
2023-11-21 07:25:30 +0000

    stream load dataset and process

--- training/codegen_rust.py
+++ training/codegen_rust.py
@@ -1,14 +1,20 @@
+"""command to run the py file:
+    torchrun --nproc_per_node=7  ./training/codegen_rust.py
+"""
+
 from transformers import AutoTokenizer, AutoModelForCausalLM,Trainer, TrainingArguments, AutoConfig
 from datasets import load_dataset
 import os
 import torch
 import torch.distributed as dist
+
+
 #clean CUDA cache and define using cuda
 torch.cuda.empty_cache()
-# os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 #define the gpu backend to launch training
-# dist.init_process_group(backend="nccl")
+dist.init_process_group(backend="nccl")
+
 
 #train the models
 
@@ -16,99 +22,54 @@ torch.cuda.empty_cache()
 tokenizer = AutoTokenizer.from_pretrained("Salesforce/codegen-2B-multi")
 model = AutoModelForCausalLM.from_pretrained("Salesforce/codegen-2B-multi")
 
-# class CustomModel(AutoModelForCausalLM):
-#     def __init__(self, config):
-#         super().__init__(config)
-
-#     def forward(self, input_ids, attention_mask, labels=None):
-#         outputs = super().forward(input_ids=input_ids, attention_mask=attention_mask, labels=input_ids)
-#         loss = outputs.loss
-#         return loss
-
-#     @classmethod
-#     def from_pretrained(cls, pretrained_model_name_or_path, *model_args, **kwargs):
-#         config = kwargs.pop('config', None)
-#         if config is None:
-#             config = AutoConfig.from_pretrained(pretrained_model_name_or_path, *model_args, **kwargs)
-#         model = super().from_pretrained(pretrained_model_name_or_path, *model_args, config=config, **kwargs)
-#         return model
-    
-# #use self-defined model inheriting from AutoModelForCausalLM
-# model = CustomModel.from_pretrained("Salesforce/codegen-2B-multi")
-
-
-
-# train_dataset = load_dataset('text', data_files='./training/data/train_dataset.txt')['train']
-# valid_dataset = load_dataset('text', data_files='./training/data/valid_dataset.txt')['train']
-# test_dataset = load_dataset('text', data_files='./training/data/test_dataset.txt')['train']
-
+#stream loading data
+processed_data_folder = './training/data'
 
 #apply tokenizer
 tokenizer.add_special_tokens({'pad_token': '[PAD]'})
 def encode(examples):
-    encoded = tokenizer(examples['text'], truncation=True, padding='max_length', max_length=256, return_tensors='pt')
+    encoded = tokenizer(examples, truncation=True, padding='max_length', max_length=512, return_tensors='pt')
     encoded['labels'] = encoded['input_ids'].clone()
     return encoded
 
+def load_data_in_batches(folder, batch_size):
+    batch_data = []
+    for filename in os.listdir(folder):
+        file_path = os.path.join(folder, filename)
+        with open(file_path, 'r') as file:
+            content = file.read()
+            encoded = encode(content)
+            batch_data.append(encoded)
+            if len(batch_data) == batch_size:
+                yield batch_data
+                batch_data = []
+    if batch_data:
+        yield batch_data
 
-# train_dataset = train_dataset.map(encode, batched=True)
-# valid_dataset = valid_dataset.map(encode, batched=True)
-# test_dataset = test_dataset.map(encode, batched=True)
-
-# train_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
-# valid_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
-# test_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
-
-training_args = TrainingArguments(
-    output_dir='./training/results',            
-    num_train_epochs=3,                         
-    per_device_train_batch_size=1,              # 每个设备的训练批量大小
-    per_device_eval_batch_size=1,               # 每个设备的评估批量大小
-    no_cuda=False,  
-    warmup_steps=500,                           
-    weight_decay=0.01,                          
-    logging_dir='./logs',                       
-    logging_steps=10,
-    local_rank=-1
-)
-
-
-data_files = {
-    'train': [f'./training/batch_data/train_dataset{i}.txt' for i in range(1, 30)],
-    'valid': [f'./training/batch_data/valid_dataset{i}.txt' for i in range(1, 30)],
-    'test': [f'./training/batch_data/test_dataset{i}.txt' for i in range(1, 30)]
-}
-
-for train_file, valid_file in zip(data_files['train'], data_files['valid']):
-    
-    train_dataset = load_dataset('text', data_files=train_file)['train']
-    valid_dataset = load_dataset('text', data_files=valid_file)['train']
-    
-    train_dataset = train_dataset.map(encode, batched=True)
-    valid_dataset = valid_dataset.map(encode, batched=True)
-
-    train_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
-    valid_dataset.set_format('torch', columns=['input_ids', 'attention_mask', 'labels'])
+# Use yield to load data batch by batch
+for batch in load_data_in_batches(processed_data_folder, batch_size=16):
+    training_args = TrainingArguments(
+        output_dir='./training/results',            
+        num_train_epochs=3,                         
+        per_device_train_batch_size=1,              
+        per_device_eval_batch_size=1,               
+        no_cuda=False,  
+        warmup_steps=500,                           
+        weight_decay=0.01,                          
+        logging_dir='./logs',                       
+        logging_steps=10,
+        local_rank=-1,
+        max_steps=10000
+    )
 
     trainer = Trainer(
         model=model,
         args=training_args,
-        train_dataset=train_dataset,
-        eval_dataset=valid_dataset
+        train_dataset=batch,
+        # eval_dataset=valid_dataset
     )
 
     trainer.train()
 
-trainer.save_model("./training/final_saved_model")
-
-# trainer = Trainer(
-#     model=model,
-#     args=training_args,
-#     train_dataset=train_dataset,
-#     eval_dataset=valid_dataset
-# )
-
-# trainer.train()
-
-# trainer.save_model("./training/saved_model")
-results = trainer.evaluate(test_dataset)
+    trainer.save_model("./training/saved_model")
+    # results = trainer.evaluate(test_dataset)
--- training/data_divided.py
+++ training/data_divided.py
@@ -1,36 +1,34 @@
 import os
 import jsonlines
-from sklearn.model_selection import train_test_split
+# from sklearn.model_selection import train_test_split
 
 #load datasets
 
 source_codes = []
-folder_path = './training/source'
+source_folder = './training/source'
+output_folder = './training/data'
 
-
-for filename in os.listdir(folder_path):
-    if filename.endswith('.jsonl'):
-        file_path = os.path.join(folder_path, filename)
-
+for filename in os.listdir(source_folder):
+    if filename.endswith('success.jsonl'):
+        file_path = os.path.join(source_folder, filename)
         with jsonlines.open(file_path) as reader:
-            for obj in reader:
-                # process every JSON object
-                source_code = obj.get('test','')
-                if source_code:
-                    source_codes.append(source_code)
+            for i,obj in enumerate(reader):
+                output_file = os.path.join(output_folder, f"{os.path.basename(file_path)}_{i}.txt")
+                with open(output_file, 'w') as f:
+                    f.write(obj['code'] + "\n" + obj['test'])
 
 
 
-# divided the datasets
-train_data, temp_data = train_test_split(source_codes, test_size=0.3, random_state=42)
-valid_data, test_data = train_test_split(temp_data, test_size=0.5, random_state=42)
+# # divided the datasets
+# train_data, temp_data = train_test_split(source_codes, test_size=0.3, random_state=42)
+# valid_data, test_data = train_test_split(temp_data, test_size=0.5, random_state=42)
 
-#save data
-def save_data(data, filename):
-    with open(filename, 'w') as file:
-        for item in data:
-            file.write(item + '\n')
+# #save data
+# def save_data(data, filename):
+#     with open(filename, 'w') as file:
+#         for item in data:
+#             file.write(item + '\n')
 
-save_data(train_data, './training/data/train_dataset.txt')
-save_data(valid_data, './training/data/valid_dataset.txt')
-save_data(test_data, './training/data/test_dataset.txt')
+# save_data(train_data, './training/data/train_dataset.txt')
+# save_data(valid_data, './training/data/valid_dataset.txt')
+# save_data(test_data, './training/data/test_dataset.txt')
