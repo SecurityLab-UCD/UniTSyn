2b0dcbb1e744fce1a5293db569f1adad65956ce3
Yifeng He <yfhe@ucdavis.edu>
2023-11-21 17:53:55 -0800

    add Golang frontend

---
+++ frontend/go/collect_all.py
@@ -0,0 +1,145 @@
+from typing import Iterable
+import fire
+import os
+from tree_sitter.binding import Node
+from frontend.parser import GO_LANGUAGE
+from frontend.parser.ast_util import ASTUtil
+from returns.maybe import Maybe, Nothing, Some
+from unitsyncer.util import replace_tabs
+import json
+from frontend.util import mp_map_repos, wrap_repo, run_with_timeout
+from collections import Counter
+from frontend.go.collect_focal import get_focal_call, is_test_fn
+
+
+def has_test(file_path):
+    with open(file_path, "r", errors="replace") as f:
+        code = f.read()
+
+    return '"testing"' in code
+
+
+def collect_test_files(root: str):
+    """Get all files end with .java in the given root directory
+
+    Args:
+        root (str): path to repo root
+    """
+    for dirpath, _, filenames in os.walk(root):
+        for filename in filenames:
+            if filename.endswith(".go") and "test" in filename:
+                if has_test(p := os.path.join(dirpath, filename)):
+                    yield p
+
+
+def collect_test_funcs(ast_util: ASTUtil) -> Iterable[Node]:
+    """collect testing functions from the target file"""
+
+    tree = ast_util.tree(GO_LANGUAGE)
+    root_node = tree.root_node
+
+    decls = ast_util.get_all_nodes_of_type(root_node, "function_declaration")
+
+    return filter(lambda n: is_test_fn(n, ast_util), decls)
+
+
+def collect_test_n_focal(file_path: str):
+    with open(file_path, "r", errors="replace") as f:
+        ast_util = ASTUtil(replace_tabs(f.read()))
+
+    def get_focal_for_test(test_func: Node):
+        test_name = ast_util.get_method_name(test_func).value_or(None)
+        focal, focal_loc = get_focal_call(ast_util, test_func).value_or((None, None))
+        return {
+            "test_id": test_name,
+            "test_loc": test_func.start_point,
+            "test": ast_util.get_source_from_node(test_func),
+            "focal_id": focal,
+            "focal_loc": focal_loc,
+        }
+
+    return map(get_focal_for_test, collect_test_funcs(ast_util))
+
+
+@run_with_timeout
+def collect_from_repo(repo_id: str, repo_root: str, test_root: str, focal_root: str):
+    """collect all test functions in the given project
+    return (status, nfile, ntest)
+    status can be 0: success, 1: repo not found, 2: test not found, 3: skip when output file existed
+    """
+    repo_path = os.path.join(repo_root, wrap_repo(repo_id))
+    if not os.path.exists(repo_path) or not os.path.isdir(repo_path):
+        return 1, 0, 0
+    test_path = os.path.join(test_root, wrap_repo(repo_id) + ".txt")
+    focal_path = os.path.join(focal_root, wrap_repo(repo_id) + ".jsonl")
+    # skip if exist
+    if os.path.exists(focal_path):
+        return 3, 0, 0
+    # collect potential testing modules
+    all_files = collect_test_files(repo_path)
+    all_files = list(all_files)
+    tests = {}
+    for f in all_files:
+        funcs = collect_test_n_focal(f)
+        tests[f] = funcs
+
+    if len(tests.keys()) == 0:
+        return 2, 0, sum(len(list(v)) for v in tests.values())
+    # save to disk
+    n_test_func = 0
+    n_focal_func = 0
+    with open(focal_path, "w") as outfile:
+        for k in tests.keys():
+            for d in tests[k]:
+                test_id = f"{k.removeprefix(repo_root)}::{d['test_id']}"
+                d["test_id"] = test_id[1:] if test_id[0] == "/" else test_id
+                if d["focal_loc"] is None:
+                    continue
+                outfile.write(json.dumps(d) + "\n")
+                n_test_func += int(d["test_loc"] is not None)
+                n_focal_func += int(d["focal_loc"] is not None)
+    return 0, n_test_func, n_focal_func
+
+
+def main(
+    repo_id: str = "mistifyio/go-zfs",
+    repo_root: str = "data/repos/",
+    test_root: str = "data/tests/",
+    focal_root: str = "data/focal/",
+    timeout: int = 120,
+    nprocs: int = 0,
+    limits: int = -1,
+):
+    try:
+        repo_id_list = [l.strip() for l in open(repo_id, "r").readlines()]
+    except:
+        repo_id_list = [repo_id]
+    if limits > 0:
+        repo_id_list = repo_id_list[:limits]
+    print(f"Loaded {len(repo_id_list)} repos to be processed")
+
+    # collect focal function from each repo
+    status_ntest_nfocal = mp_map_repos(
+        collect_from_repo,
+        repo_id_list=repo_id_list,
+        nprocs=nprocs,
+        timeout=timeout,
+        repo_root=repo_root,
+        test_root=test_root,
+        focal_root=focal_root,
+    )
+
+    filtered_results = [i for i in status_ntest_nfocal if i is not None]
+    if len(filtered_results) < len(status_ntest_nfocal):
+        print(f"{len(status_ntest_nfocal) - len(filtered_results)} repos timeout")
+    status, ntest, nfocal = zip(*filtered_results)
+    status = Counter(status)
+    print(
+        f"Processed {sum(status.values())} repos with {status[3]} skipped, {status[1]} not found, and {status[2]} failed to locate any focal functions"
+    )
+    print(f"Collected {sum(nfocal)} focal functions for {sum(ntest)} tests")
+    print("Done!")
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
---
+++ frontend/go/collect_focal.py
@@ -0,0 +1,101 @@
+import fire
+import re
+from tree_sitter.binding import Node
+from frontend.parser import GO_LANGUAGE
+from frontend.parser.ast_util import ASTUtil, ASTLoc, flatten_postorder
+from returns.maybe import Maybe, Nothing, Some, maybe
+
+
+def get_focal_call(ast_util: ASTUtil, func: Node) -> Maybe[tuple[str, ASTLoc]]:
+    """Find the focal call in the given function
+
+    Args:
+        ast_util (ASTUtil): ASTUtil for the file
+        func (Node): a method_declaration node
+
+    Returns:
+        Maybe[tuple[str, ASTLoc]]: focal call and its location
+    """
+
+    # todo: find better heuristic to match object on imports
+    """get the focal call from the given function"""
+
+    calls = flatten_postorder(func, "call_expression")
+
+    def get_basename(call: Node) -> str:
+        func_name = ast_util.get_all_nodes_of_type(call, "identifier")[0]
+        return ast_util.get_source_from_node(func_name)
+
+    calls_before_assert: list[tuple[str, Node]] = []
+    has_assert = False
+    for call in calls:
+        base_name = get_basename(call)
+        if base_name in {"ok", "equals", "assert"}:
+            has_assert = True
+            break
+
+        full_name = ast_util.get_source_from_node(call).split("(")[0]
+        calls_before_assert.append((full_name, call))
+
+    if not has_assert or calls_before_assert == []:
+        return Nothing
+
+    # todo: check if focal is imported from workdir package
+    def get_loc(n: tuple[str, Node]) -> tuple[str, ASTLoc]:
+        full_name, node = n
+        lineno, col = node.start_point
+        match full_name.split("."):
+            case [obj_name, *_, method_name]:
+                offset = len(full_name) - len(method_name)
+                return method_name, (lineno, col + offset)
+            case _:
+                return full_name, (lineno, col)
+
+    return Some(get_loc(calls_before_assert[-1]))
+
+
+def is_test_fn(node: Node, ast_util: ASTUtil) -> bool:
+    """check if the function is a Golang test function
+
+    Golang requires a test function to have `t *testing.T` input parameter
+
+    Args:
+        node (Node): a function_delc node
+        ast_util (ASTUtil): node's util object
+
+    Returns:
+        bool: true if node is a unit test function, otherwise not
+    """
+    # func delc always have parameter_list
+    params = ast_util.get_all_nodes_of_type(node, "parameter_list")[0]
+    param_types = ast_util.get_all_nodes_of_type(params, "qualified_type")
+
+    return "testing.T" in map(ast_util.get_source_from_node, param_types)
+
+
+def main():
+    code = """func TestDatasets(t *testing.T) {
+	defer setupZPool(t).cleanUp()
+
+	_, err := zfs.Datasets("")
+	ok(t, err)
+
+	ds, err := zfs.GetDataset("test")
+	ok(t, err)
+	equals(t, zfs.DatasetFilesystem, ds.Type)
+	equals(t, "", ds.Origin)
+	if runtime.GOOS != "solaris" {
+		assert(t, ds.Logicalused != 0, "Logicalused is not greater than 0")
+	}
+}"""
+
+    ast_util = ASTUtil(code)
+    tree = ast_util.tree(GO_LANGUAGE)
+    root_node = tree.root_node
+
+    fn = ast_util.get_all_nodes_of_type(root_node, "function_declaration")[0]
+    print(get_focal_call(ast_util, fn))
+
+
+if __name__ == "__main__":
+    fire.Fire(main)
