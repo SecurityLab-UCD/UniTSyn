e1a89cb8ce5a21ea308865fdd2d0a9816c27d67c
Yifeng He <yfhe@ucdavis.edu>
2023-12-04 14:30:11 -0800

    rm extra %

--- evaluation/rust/rust_test_coverage.sh
+++ evaluation/rust/rust_test_coverage.sh
@@ -3,4 +3,5 @@
 TARGET=$1
 RUSTFLAGS="-C instrument-coverage" LLVM_PROFILE_FILE="$TARGET-%p-%m.profraw" cargo test $TARGET &> /dev/null
 grcov $TARGET*.profraw -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/$TARGET
-rm $TARGET*.profraw%
+rm $TARGET*.profraw
+
