14a2f846a59d75584048a9b16ef4a60640621cdc
Yifeng He <yfhe@ucdavis.edu>
2023-12-03 17:42:34 -0800

    add script to get cargo test coverage

---
+++ scripts/rust_test_coverage.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+TARGET=$1
+RUSTFLAGS="-C instrument-coverage" LLVM_PROFILE_FILE="$TARGET-%p-%m.profraw" cargo test $TARGET &> /dev/null
+grcov $TARGET*.profraw -s . --binary-path ./target/debug/ -t html --branch --ignore-not-existing -o ./target/debug/coverage/$TARGET
+rm $TARGET*.profraw%
