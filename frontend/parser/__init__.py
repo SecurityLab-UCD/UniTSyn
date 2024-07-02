7a4a57829edc23956c8de84ad5015d16457b71f4
lengsaoman <93131531+lengsaoman@users.noreply.github.com>
2024-07-02 14:28:10 +0800

    Fix python maybe work

--- frontend/parser/__init__.py
+++ frontend/parser/__init__.py
@@ -3,6 +3,7 @@ import tree_sitter_rust as tsrust
 import tree_sitter_java as tsjava
 import tree_sitter_javascript as tsjavascript
 import tree_sitter_go as tsgo
+import tree_sitter_python as python
 import tree_sitter_cpp as tscpp
 
 JAVA_LANGUAGE = Language(tsjava.language(), "java")
@@ -10,3 +11,4 @@ JAVASCRIPT_LANGUAGE = Language(tsjavascript.language(), "javascript")
 RUST_LANGUAGE = Language(tsrust.language(), "rust")
 GO_LANGUAGE = Language(tsgo.language(), "go")
 CPP_LANGUAGE = Language(tscpp.language(), "cpp")
+PYTHON_LANGUAGE = Language(python.language(), "python")
